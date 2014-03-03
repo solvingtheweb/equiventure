e52c8937ef55bd7564bbb96bcaa0efc8682e92e8
Chris Macholz <chris@solvingtheweb.com>
2014-03-02 20:29:42 -0500

    Added global header include and removed old header. Removed some broken links.

--- financialresearch.php
+++ financialresearch.php
@@ -1,70 +1,6 @@
-<head><title>Equiventure Capital </title>
-<script language="javascript">AC_FL_RunContent = 0;</script>
-<script src="AC_RunActiveContent.js" language="javascript"></script>
-</head>
-<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+<?php include("header.php"); ?>
 
-<body>
-	<div id="headercontainer">
-	<table>
-	<tr>
-	<td>
-	
-	<script language="javascript">
-	if (AC_FL_RunContent == 0) {
-		alert("This page requires AC_RunActiveContent.js.");
-	} else {
-		AC_FL_RunContent(
-			'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0',
-			'width', '190',
-			'height', '80',
-			'src', 'Logo_build_01',
-			'quality', 'high',
-			'pluginspage', 'http://www.macromedia.com/go/getflashplayer',
-			'align', 'middle',
-			'play', 'true',
-			'loop', 'true',
-			'scale', 'showall',
-			'wmode', 'window',
-			'devicefont', 'false',
-			'id', 'Logo_build_01',
-			'bgcolor', '#ffffff',
-			'name', 'Logo_build_01',
-			'menu', 'true',
-			'allowFullScreen', 'false',
-			'allowScriptAccess','sameDomain',
-			'movie', 'Logo_build_01',
-			'salign', ''
-			); //end AC code
-		}
-	</script>
-	<noscript>
-	<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="237" height="100" id="Logo_build_01" align="middle">
-	<param name="allowScriptAccess" value="sameDomain" />
-	<param name="allowFullScreen" value="false" />
-	<param name="movie" value="Logo_build_01.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" />	<embed src="Logo_build_01.swf" quality="high" bgcolor="#ffffff" width="237" height="100" name="Logo_build_01" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-	</object>
-	</noscript>
-	</td>
-	<td class="menu" valign="bottom">
-		<a href="index.php">Home</a>
-		<a href="overview.php">About Us</a>
-		<a href="news.php">EVC News</a>
-		<a href="programs.php">Loan Programs</a>
-		<a href="submitproject.php">Project Submission</a>
-		<a href="projects.php">Current Projects</a>
-		<a href="residential.php">Residential</a>
-		<a href="contact.php">Contact</a>
-	</td>
-
-	</tr>
-	</table>	
-	</div>	
-	</div>
-
-	
 	 <div id="container">
-		
 
 		<div id="top3">
 		<img src="images/banner_sub.jpg" class="bannershadow">
@@ -104,14 +40,6 @@
 
 		</tr>
 		<tr>
-			<td height="15" width="400">
-			<a target='_new' class='bio' href="http://www.mortgageoriginator.com/FSG/MortgageOriginator/default.htm">Mortgage Originator Magazine</a>
-			</td>
-			<td height="15" width="400">
-			<a target='_new' class='bio' href="http://www.firstpreston.com">HUD Foreclosure Properties and Bidding</a>
-			</td>
-		</tr>
-		<tr>
 			<td height="25" width="400">
 			<a target='_new' class='bio' href="http://www.mortgagepress.com/">The Mortgage Press Newspaper</a>
 			</td>
@@ -145,7 +73,7 @@
 		</tr>
 		<tr>
 			<td height="25" width="400">
-			<a target='_new' class='bio' href="http://www.moneyline.com">Money Line Live US Treasury Data</a>
+			<a target='_new' class='bio' href="http://www.firstpreston.com">HUD Foreclosure Properties and Bidding</a>
 			</td>
 			<td height="25" width="400">
 			<a target='_new' class='bio' href="http://www.banking.state.ny.us">New York State Banking Department</a>
